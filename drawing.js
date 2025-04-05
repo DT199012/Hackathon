23d1f59454a32c55769cbd03b95268fcceae9bff
Ps-Tl-lp <prithvisuresh2006@gmail.com>
2025-04-05 15:42:18 -0700

    Adding more changes

--- drawing.js
+++ drawing.js
@@ -16,3 +16,20 @@ theInput.addEventListener("input", function(){
     theColor = theInput.value;
     body.style.backgroundColor = theColor;
 }, false);
+
+const ctx = canvas.getContext("2d");
+ctx.lineWidth = lineW;
+
+document.getElementById("ageInputId").oninput = function() {
+    lineW = document.getElementById("ageInputId").value;
+    document.getElementById("ageOutputId").innerHTML = lineW;
+    ctx.lineWidth = lineW;
+};
+
+let clrs = document.querySelectorAll(".clr");
+clrs = Array.from(clrs);
+clrs.forEach(clr => {
+    clr.addEventListener("click", () => {
+        ctx.strokeStyle = clr.getAttribute("data-clr");
+    });
+});
